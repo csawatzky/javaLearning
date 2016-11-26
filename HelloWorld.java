b4e847fa8b2d24b7ac14e818f054f9dc230978c6
Catherine <aelfscine@gmail.com>
2016-11-26 15:47:36 -0500

    first java file

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld{
+ 
+  public static void main(String[] args){
+
+	  String bestshow = "The best show is..(drumroll)Dr.Who, I guess........";
+
+	  for (String arg : args){
+	  	 System.out.println("Arg matey");
+	  	}
+
+	  
+
+	  for (int i=0; i<9; i++) {
+	   System.out.println("Arg matey");
+
+	  
+      }
+  }
+
+}
